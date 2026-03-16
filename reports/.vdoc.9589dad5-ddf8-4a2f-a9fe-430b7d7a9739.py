a9d63e7cbcd3981f5d4d6cff71392d17ceb43013
atao25 <allisontao04@gmail.com>
2026-03-15 21:12:35 -0700

    Complete IA4 Quarto report

---
+++ reports/.vdoc.9589dad5-ddf8-4a2f-a9fe-430b7d7a9739.py
@@ -0,0 +1,83 @@
+# type: ignore
+# flake8: noqa
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+import pandas as pd
+from IPython.display import Markdown, display
+from tabulate import tabulate
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+horses_sd_table = pd.read_csv("../results/horses_sd.csv")
+largest_sd = horses_sd_table["Province"].values[0]
+horses_sd_table
+# horses_sd_table = pd.read_csv("../results/horses_sd.csv")
+# largest_sd = horses_sd_table['Province'].values[0]
+# Markdown(horses_sd_table.to_markdown(index = False))
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
